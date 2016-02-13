912ffb0b2f79ffd1512f976bbbc6548949aff793
roland <rsx@bluewin.ch>
2016-02-13 19:48:54 +0100

    Do not use tasty anymore

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,67 +1,88 @@
-import Test.Tasty
-import Test.Tasty.QuickCheck
-import Test.Tasty.HUnit
-
 import Data.Geo.Swiss.Conversion
 
-{-
-instance Arbitrary Degree where 
-  arbitrary =  do
-       d <- choose (0, 90)
-       m <- choose (0, 59)
-       s <- choose (0, 59.99)
-       return $ Deg d m (round2digs s)
--}
+import Text.Printf
+import Control.Monad
+import Test.QuickCheck
+import Test.QuickCheck.Test
+import System.Exit
+
+main :: IO()
+main  = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
+
+tests :: [(String, IO())]
+tests = [ ("testRigi", runTest testRigi)
+        , ("testBern", runTest testBern)
+        , ("prop_ch95", runTest prop_ch95)
+        , ("prop_ch95HP", runTest prop_ch95hp)]
+
+runTest :: Testable prop => prop -> IO ()
+runTest prop = do
+  result <- quickCheckWithResult myArgs prop
+  unless (isSuccess result) exitFailure
+  return ()
+
+-- | My default test arguments
+myArgs :: Args
+myArgs = Args
+  { replay          = Nothing
+  , maxSuccess      = 1000
+  , maxDiscardRatio = 10
+  , maxSize         = 1000
+  , chatty          = True
+-- noShrinking flag?
+  }
 
 -- | Make datatype CH95 eligible for testing with QuickCheck
 instance Arbitrary CH95 where
-  arbitrary =  do    
-       x <- choose(2480000, 2840000) 
+  arbitrary =  do
+       x <- choose(2480000, 2840000)
        y <- choose(1070000, 1300000)
        return $ LV95 x y
 
--- | 
+-- | Make datatype CH95HP eligible for testing with QuickCheck
 instance Arbitrary CH95HP where
-  arbitrary = do 
-       x <- choose(2480000, 2840000) 
+  arbitrary = do
+       x <- choose(2480000, 2840000)
        y <- choose(1070000, 1300000)
        return $ LV95HP (round2digs x) (round2digs y)
 
-main :: IO()
-main = defaultMain tests
-
--- | All our tests: We have properties tests and unitTests
-tests :: TestTree
-tests = testGroup "Tests" [properties, unitTests]
-
--- | The property tests we do with 10000 test cases
-properties :: TestTree
-properties = localOption (QuickCheckTests 10000) $ testGroup "Properties" [qcProps]
-
-qcProps :: TestTree
-qcProps = testGroup "checked by QuickCheck"
-           [testProperty "LV95" prop_ch95, 
-            testProperty "LV95HP" prop_ch95hp]
-
 {-
--- | Property does not yet hold, not enough precision
-prop_degree x = (rad2deg . deg2rad) x == x 
-   where types = x :: Degree
+instance Arbitrary Degree where
+  arbitrary =  do
+    d <- choose (0, 90)
+    m <- choose (0, 59)
+    s <- choose (0, 59.99)
+    return $ Deg d m (round2digs s)
 -}
+
+-- ----------------------------------------------------------------------------
+-- Unit Tests
+-- ----------------------------------------------------------------------------
+testRigi :: Bool
+testRigi = wgs2chHP wgsRigi == chRigi
+
+testBern ::  Bool
+testBern = wgs2ch wgsBern == chBern
+
+-- ----------------------------------------------------------------------------
+-- Property Tests
+-- ----------------------------------------------------------------------------
 
 -- | The easy ones: convert Swiss CH95 coordinates to world coordinates
 --   and back again. Here we have a precision of 1 meter
+prop_ch95 :: CH95 -> Bool
 prop_ch95 x = (wgs2ch . ch2wgs) x == x
-   where types = x :: CH95 
 
 -- | The exacts: convert Swiss CH95 hight precision coordinates to world coordinates
 --   and back again. Here we have a precision of 1 cm
+prop_ch95hp :: CH95HP -> Bool
 prop_ch95hp x = (wgs2chHP . ch2wgsHP) x == x
-   where types = x :: CH95HP
 
-unitTests = testGroup "Unit tests"
-   [testCase "Rigi" $ wgs2chHP wgsRigi @?= chRigi,
-    testCase "Bern" $ wgs2ch wgsBern @?= chBern]
+{-
+-- | Property does not yet hold, not enough precision
+prop_degree x = (rad2deg . deg2rad) x == x
+   where types = x :: Degree
+-}
 
 -- ----------------------------------------------------------------------------
 --  Some data for unit tests
